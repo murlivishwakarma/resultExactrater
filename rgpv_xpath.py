41457cdad7a99456eee911e9a6b13a8e65823e96
Murli Vishwakarma <134859656+murlivishwakarma@users.noreply.github.com>
2024-10-14 20:37:44 +0530

    Create rgpv_xpath.py

---
+++ rgpv_xpath.py
@@ -0,0 +1,23 @@
+course_click_xp = '//*[@id="radlstProgram_1"]'
+
+captcha_xp = '/html/body/form/div[3]/div/div[2]/table/tbody/tr[5]/td/div/table/tbody/tr[1]/td/div/img' 
+
+enroll_xp = '/html/body/form/div[3]/div/div[2]/table/tbody/tr[4]/td/table/tbody/tr/th[2]/input'
+
+sem_ddn_xp = '/html/body/form/div[3]/div/div[2]/table/tbody/tr[4]/td/table/tbody/tr/th[4]/select'
+
+sem_sel_xp = '/html/body/form/div[3]/div/div[2]/table/tbody/tr[4]/td/table/tbody/tr/th[4]/select/option[4]'
+
+captcha_input_xp = '/html/body/form/div[3]/div/div[2]/table/tbody/tr[5]/td/div/table/tbody/tr[2]/th/input'
+
+submit_xp = '/html/body/form/div[3]/div/div[2]/table/tbody/tr[5]/td/div/table/tbody/tr[3]/th/input'
+
+name_id = 'ctl00_ContentPlaceHolder1_lblNameGrading'
+
+roll_no_id = 'ctl00_ContentPlaceHolder1_lblRollNoGrading'
+
+reset_xp = '//*[@id="ctl00_ContentPlaceHolder1_btnReset"]'
+
+sgpa_id = 'ctl00_ContentPlaceHolder1_lblSGPA'
+
+cgpa_id = 'ctl00_ContentPlaceHolder1_lblcgpa'
